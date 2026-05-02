8c0b7b2845e8a3da0028b02787a417604e0ebc22
gulaix <gns161202@gmail.com>
2026-05-02 19:14:53 +0200

    4> adsr envelope
    feat: implementato inviluppo ADSR real-time
    
    - Aggiunto motore ADSR (Attack, Decay, Sustain, Release) per modellare l'ampiezza nel tempo.
    - Implementata macchina a stati (IDLE, ATTACK, DECAY, SUSTAIN, RELEASE) calcolata iterativamente campione per campione all'interno del buffer audio.
    - La callback ora restituisce l'onda grezza moltiplicata per il buffer dell'inviluppo generato, permettendo la gestione dinamica del volume tramite eventi di NOTE ON / NOTE OFF simulati nel loop principale.

--- Vibeditor.py
+++ Vibeditor.py
@@ -3,52 +3,105 @@ import numpy as np
 import time
 
 fs = 44100
-frequenza = 440.06
-fase_attuale = 0.0  #sostituisce campioni_totali = 0   
+frequency = 440.00
+current_phase = 0.0
+wave_type = "SIN" #cambia in "SIN" "SQR" "SAW"
 
-#selettore waveshape
-tipo_onda = "SAW" #cambia in "SIN" "SQR" "SAW"
+#parametri ADSR
+attack_t = 0.001
+decay_t = 0.5
+sustain_lvl = 0.4
+release_t = 0.8
 
-def process_audio (outdata, frames, time_info, status): #funzione callback --> scheda audio
-    global fase_attuale, frequenza, tipo_onda
+#variabili di stato
+current_amplitude = 0.0
+envelope_state = "IDLE"  # IDLE, ATTACK, DECAY, SUSTAIN, RELEASE
+
+def process_audio (outdata, frames, time_info, status): #funzione callback
+    global current_phase, frequency, wave_type
+    global current_amplitude, envelope_state
     
     #calcoliamo gli step di fase in base a frequenza attuale
-    incremento_fase = 2 * np.pi * frequenza / fs #Δϕ=2π⋅f/fs | f=cicli/s fs=campioni/s |
+    phase_increment = 2 * np.pi * frequency / fs #Δϕ=2π⋅f/fs | f=cicli/s fs=campioni/s |
                                                  #--analisi_dimens--> [f]/[fs]=cicli/campioni |
                                                  # si fa ⋅2π per trasformare in RAD
 
     #array degli step del buffer assegnato a passi [0, 1, 2... 511]
-    passi = np.arange(frames)
+    steps = np.arange(frames)
 
     #calcoliamo fase  di ogni step (a diff. di prima ora normalizzo array di fasi in [0,2π])
-    fasi = (fase_attuale + (passi * incremento_fase)) % (2 * np.pi) #SAW e SQR richiedonou una
-                                                                    #fase strettamente limitata
-                                                                    #per generare fronti di discesa
-    # --- BLOCCO SELETTORI FORME D'ONDA --- #                       #corretti
+    phases = (current_phase + (steps * phase_increment)) % (2 * np.pi) #SAW e SQR richiedonou una
+                                                                       #fase strettamente limitata
+                                                                       #per generare fronti di discesa
+    # --- BLOCCO SELETTORI FORME D'ONDA --- #                          #corretti
 
-    if tipo_onda == "SIN":
-        onda = np.sin(fasi)
+    if wave_type == "SIN":
+        wave = np.sin(phases)
 
-    elif tipo_onda == "SAW":
+    elif wave_type == "SAW":
         # 0<=fasi<=2π --> :π --> 0<=fasi<=2 --> -1 --> -1<=fasi<=1
         #inoltre all'aumento delle fasi la funzione cresce linearmente 
         #fino ad 1 (2π), crollando a -1 superato i 2π
-        onda = (fasi / np.pi) - 1.0
+        wave = (phases / np.pi) - 1.0
 
-    elif tipo_onda == "SQR":
+    elif wave_type == "SQR":
         # [fasi<π] --> onda=1    [else] --> onda=-1
-        onda = np.where(fasi < np.pi, 1.0, -1.0)
+        wave = np.where(phases < np.pi, 1.0, -1.0)
 
     else:
-        onda = np.zeros(frames)
-        
+        wave = np.zeros(frames) #preveniamo errore: tutto a zero
+
+
+    # --- motore ADSR --- #
+    #calcoliamo di quanto deve cambiare il volume per ogni campione
+    attack_delta = 1.0 / (0.001 * fs) #NUM: (1.0 - 0) = 1.0 --> max_da_raggiungere
+                                                     #DEN: tempo_di_attacco * 44100Hz
+
+    decay_delta = (1.0 - sustain_lvl) / (0.001 * fs) #(1.0 - sustain_lvl) --> per arrivare al sustain
+
+    release_delta = sustain_lvl / (0.001 * fs)
+
+    envelope_buffer = np.zeros(frames) #envelope_buffer[i] per ciclo for
+
+# --- CICLO FOR: ADSR CAMPIONE PER CAMPIONE --- #
+    # perche usare un for --> riempire envelope_buffer uno step alla volta [i=0, 1... 511]
+    # questo ci permette di cambiare stat (es: ATTACK --> DECAY) nell'istante esatto 
+    # in cui tocchiamo il limite (es: 1.0), anche se succede esattamente a metà del buffer
+    # [se calcolassimo tutto il blocco in una volta sola, non potremmo cambiare pendenza a metà]
+    for i in range(frames):
+        if envelope_state == "ATTACK":
+            current_amplitude += attack_delta
+            if current_amplitude >= 1.0:
+                current_amplitude = 1.0
+                envelope_state = "DECAY"
+
+        elif envelope_state == "DECAY":
+            current_amplitude -= decay_delta
+            if current_amplitude <= sustain_lvl:
+                current_amplitude = sustain_lvl
+                envelope_state = "SUSTAIN"
+
+        elif envelope_state == "SUSTAIN":
+            pass
+
+        elif envelope_state == "RELEASE":
+            current_amplitude -= release_delta
+            if current_amplitude <= 0.0:
+                current_amplitude = 0.0
+                envelope_state = "IDLE"
+
+        envelope_buffer[i] = current_amplitude
+
+    final_wave = wave * envelope_buffer
+
     #dopo aver riempito il buffer, salviamo la fase attuale sommata ai 512 Δϕ
     #pronta ad essere utilizzata per il prossimo buffer.
     # -- % (2 * np.pi) --  per evitare che il numero non incrementi a dismisura
-    fase_attuale = (fase_attuale + frames * incremento_fase) % (2 * np.pi)
+    current_phase = (current_phase + frames * phase_increment) % (2 * np.pi)
 
     #formatto
-    outdata[:] = onda.reshape(-1, 1).astype(np.float32)
+    outdata[:] = final_wave.reshape(-1, 1).astype(np.float32)
+
 
 print("SYNTH ON: CTR+C FOR SHUT OFF")
 
@@ -56,16 +109,19 @@ try:
     #apriamo stream audio passando la callback precedentemente costruita
     with sd.OutputStream(samplerate=fs, channels=1, callback=process_audio):
 
-    #loop <infinito per emulare la pressione alternata di due note
+    #loop <infinito per emulare la pressione ed il rilascio di una nota (ascolto dell'ADSR)
         while True:
-            time.sleep(0.5)
+            print("NOTE ON")
+            envelope_state = "ATTACK"
 
-            if frequenza == 440.0:
-                frequenza = 660.0   #Mi5
-            else:
-                frequenza = 440.0   #La4
+            time.sleep(1.0)
+
+            print("NOTE OFF")
+            envelope_state = "RELEASE"
+
+            time.sleep(1.0)
     #> fineloop
 
 #CTRL+C per interrompere    
 except KeyboardInterrupt:
-    print("STOPPED.")
+    print("STOPPED.") 
