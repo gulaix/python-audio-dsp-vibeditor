363b4022ae1bcb421fbf13a2b45154876c95e91d
gulaix <gns161202@gmail.com>
2026-05-02 20:32:10 +0200

    7> implement polyphony and master gain
    - Sostituita voce singola con una lista active_voices.
    - Implementata la somma dei segnali (mixer) nella callback.
    - Aggiunto master_gain per prevenire il digital clipping.
    - Aggiornato il main loop per i trigger polifonici.

--- Vibeditor.py
+++ Vibeditor.py
@@ -12,8 +12,8 @@ class Voice:  #classe che mi istanzierà ogni voce (nota) che premerò
         
         #parametri ADSR (settali a piacimento)
         self.attack_t = 0.01
-        self.decay_t = 0.5
-        self.sustain_lvl = 0.4
+        self.decay_t = 0.9
+        self.sustain_lvl = 1.0
         self.release_t = 0.8
 
     def trigger_on(self): #servirà per la pressione dei tasti
@@ -69,28 +69,47 @@ class Voice:  #classe che mi istanzierà ogni voce (nota) che premerò
 
 # --- CONFIGURAZIONE --- #
 fs = 44100
-#creiamo una singola istanza per ora (test monofonico)
-vocal_voice = Voice(440.0, fs)
-wave_type = "SAW"
+wave_type = "SQR" # | SIN | SQR | SAW
 
-def process_audio(outdata, frames, time_info, status): #callback molto snellita
-    #chiediamo alla voce di generare il suo chunk audio
-    audio_chunk = vocal_voice.render(frames, wave_type)
+#creiamo una lista di voci (La m)
+active_voices = [
+    Voice(440.00, fs),  # La 4
+    Voice(523.25, fs),  # Do 5
+    Voice(659.25, fs)   # Mi 5
+]
+def process_audio(outdata, frames, time_info, status):
+    #partiamo dal silenzio(tutti zeri)
+    mixed_audio = np.zeros(frames)
     
-    #formattazione per scheda audio
-    outdata[:] = audio_chunk.reshape(-1, 1).astype(np.float32)
+    #chiediamo a ogni voce nella lista di generare il suo chunk e lo sommiamo
+    for voice in active_voices:
+        audio_chunk = voice.render(frames, wave_type)
+        mixed_audio += audio_chunk  #mixing audio: sommare i numeri
+        
+    # --- CONTROLLO DEL VOLUME (MASTER GAIN) --- #
+    #se sommiamo 3 onde che arrivano a 1.0, il totale sarà 3.0. 
+    #oltiplichiamo per abbassare il volume (non vogliamo rompere le casse)
+    master_gain = 0.3 
+    mixed_audio = mixed_audio * master_gain
+    
+    #mandiamo il mix finale alla scheda audio
+    outdata[:] = mixed_audio.reshape(-1, 1).astype(np.float32)
 
 print("SYNTH OOP ON: CTRL+C TO STOP")
 
 try:
     with sd.OutputStream(samplerate=fs, channels=1, callback=process_audio):
         while True:
-            print("NOTE ON")
-            vocal_voice.trigger_on() #quindi quando avvio programma la callback inizia a girare, la voce è in IDLE (non suona). non appena attivo il trigger suona iniziando dall'attack
+            print("CHORD ON")
+            #accendiamole note dell'accordo
+            for voice in active_voices:
+                voice.trigger_on()
             time.sleep(1.0)
             
-            print("NOTE OFF")
-            vocal_voice.trigger_off()
+            print("CHORD OFF")
+            #spegniamo tutte le note dell'accordo
+            for voice in active_voices:
+                voice.trigger_off()
             time.sleep(1.0)
 
 except KeyboardInterrupt:
